f53368807af7aef73a30e6e34f1c223d17ab50f4
Foysal Mohammad <foysal@earlblue.com>
2025-03-07 20:42:03 +0000

    feat: configured webpack & displayed initial app to the browser

---
+++ client/src/App/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Todo list</h1>
+      </header>
+    </div>
+  );
+};
