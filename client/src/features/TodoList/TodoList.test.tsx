8eb11dc4a6d02a01ccf264cda0293a9574f0bf35
Foysal Mohammad <foysal@earlblue.com>
2025-03-08 15:34:02 +0000

    test: added test for todolist

---
+++ client/src/features/TodoList/TodoList.test.tsx
@@ -0,0 +1,40 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { TodoList } from "./TodoList";
+import { useGetTodosQuery } from "../../todosApi/todosApiSlice";
+import { vi, type Mock } from "vitest";
+import { store } from "../../redux/store";
+
+vi.mock("../../todosApi/todosApiSlice", async () => {
+  const actualModule = await vi.importActual("../../todosApi/todosApiSlice");
+  return {
+    ...actualModule,
+    useGetTodosQuery: vi.fn(),
+  };
+});
+
+describe("TodoList", () => {
+  test("renders the correct number of TodoItems based on todos data", () => {
+    const mockTodos = [
+      { _id: "1", todo: "Test Todo 1", completed: false },
+      { _id: "2", todo: "Test Todo 2", completed: false },
+      { _id: "3", todo: "Test Todo 3", completed: false },
+    ];
+
+    (useGetTodosQuery as Mock).mockReturnValue({
+      data: mockTodos,
+      isLoading: false,
+      isError: false,
+    });
+
+    render(
+      <Provider store={store}>
+        <TodoList />
+      </Provider>,
+    );
+
+    const todoItems = screen.getAllByText(/Test Todo/);
+
+    expect(todoItems).toHaveLength(mockTodos.length);
+  });
+});
