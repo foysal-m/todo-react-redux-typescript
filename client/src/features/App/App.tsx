849d41ae296644205f1ec7bddcba8e291a3bb16e
Foysal Mohammad <foysal@earlblue.com>
2025-03-08 14:36:59 +0000

    feat: added todo form

--- client/src/features/App/App.tsx
+++ client/src/features/App/App.tsx
@@ -1,8 +1,22 @@
+import { TodoForm } from "../TodoForm/TodoForm";
 import { TodoList } from "../TodoList/TodoList";
+import { type TodoType } from "../../todosApi/todosApiSlice";
 
 export const App = () => {
+  const submitAction = async (formData: FormData) => {
+    try {
+      const newTodo: Pick<TodoType, "todo"> = {
+        todo: formData.get("todo") as string,
+      };
+
+      console.log(newTodo);
+    } catch (error) {
+      return String(error);
+    }
+  };
   return (
     <div className="App">
+      <TodoForm action={submitAction} />
       <TodoList />
     </div>
   );
