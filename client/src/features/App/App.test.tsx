b246bef57d6089313d39b3345b9b60971cc12c9f
Foysal Mohammad <foysal@earlblue.com>
2025-03-08 14:58:52 +0000

    test: added test for main & app component

---
+++ client/src/features/App/App.test.tsx
@@ -0,0 +1,38 @@
+import { render } from "@testing-library/react";
+import { vi, type Mock } from "vitest";
+import { Provider } from "react-redux";
+import { App } from "./App";
+import { useGetTodosQuery } from "../../todosApi/todosApiSlice";
+import { store } from "../../redux/store";
+
+vi.mock("../../todosApi/todosApiSlice", async () => {
+  const actualModule = await vi.importActual("../../todosApi/todosApiSlice");
+  return {
+    ...actualModule,
+    useGetTodosQuery: vi.fn(),
+  };
+});
+
+describe("App Component", () => {
+  beforeEach(() => {
+    (useGetTodosQuery as Mock).mockReturnValue({
+      data: [{ _id: "1", todo: "Test Todo", completed: false }],
+    });
+  });
+
+  test("renders TodoForm and TodoList", () => {
+    const { getByRole, getByText } = render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+
+    expect(
+      getByRole("textbox", { name: "Todo input field" }),
+    ).toBeInTheDocument();
+
+    expect(getByText("Add Todo")).toBeInTheDocument();
+    expect(getByText("Test Todo")).toBeInTheDocument();
+    expect(useGetTodosQuery).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ client/src/main.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { App } from "./features/App/App";
+
+vi.mock("./features/App/App", () => ({
+  App: () => <div>Mocked App</div>,
+}));
+
+describe("App Rendering", () => {
+  test("renders the mocked App component", () => {
+    render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+    );
+    expect(screen.getByText("Mocked App")).toBeInTheDocument();
+  });
+});
