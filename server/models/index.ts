4b6232ab4433179e4086245b84b5edb84d53bac2
Foysal Mohammad <foysal@earlblue.com>
2025-03-10 08:52:09 +0000

    chore: removed unnecessary connection props & put comments for db connection

--- server/models/index.ts
+++ server/models/index.ts
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
-module.exports = mongoose.connect(process.env.DATA_BASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+module.exports = mongoose.connect(process.env.DATA_BASE_URL);
+
+// here we need a mondoDB connection string to connect to the database
+// we can get this from the MongoDB Atlas dashboard
+// the connection string is stored in the .env file
+// the connection string is accessed using process.env.DATA_BASE_URL
